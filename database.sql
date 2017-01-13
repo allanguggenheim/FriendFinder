4f24278cf27d8b6f16a8e4eb4f3f9b34c7a74e6c
Allan Guggenheim <allan.guggenheim@gmail.com>
2017-01-12 18:50:23 -0800

    heroku app v1

--- database.sql
+++ database.sql
@@ -4,12 +4,11 @@ USE heroku_b0cbb0c89762d88;
 
 CREATE TABLE `heroku_b0cbb0c89762d88`.`friends` (
     item_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(999) NULL,
-    profilePic VARCHAR(999) NULL,
-    scores VARCHAR(999) NOT NULL,
+    name VARCHAR(99) NULL,
+    profilePic VARCHAR(99) NULL,
+    scores VARCHAR(99) NOT NULL,
     PRIMARY KEY (item_id)
 );
 
 INSERT INTO `heroku_b0cbb0c89762d88`.`friends` (`item_id`, `name`, `profilePic`, `scores`) VALUES ('1', 'Cool Chihuahua', 'http://www.dog-learn.com/dog-breeds/chihuahua/images/chihuahua.jpg', '1,1,2,2,3,3,4,4,5,5');
-
 
