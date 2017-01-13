2524549854775762dfd9c4941fca82801b23be4d
Allan Guggenheim <allan.guggenheim@gmail.com>
2017-01-12 18:48:27 -0800

    heroku app v1

--- database.sql
+++ database.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE FriendFinder;
+CREATE DATABASE heroku_b0cbb0c89762d88;
 
-CREATE TABLE `FriendFinder`.`heroku_b0cbb0c89762d88` (
+USE heroku_b0cbb0c89762d88;
+
+CREATE TABLE `heroku_b0cbb0c89762d88`.`friends` (
     item_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) NULL,
     profilePic VARCHAR(45) NULL,
@@ -8,4 +10,6 @@ CREATE TABLE `FriendFinder`.`heroku_b0cbb0c89762d88` (
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO `FriendFinder`.`heroku_b0cbb0c89762d88` (`item_id`, `name`, `profilePic`, `scores`) VALUES ('1', 'Cool Chihuahua', 'http://www.dog-learn.com/dog-breeds/chihuahua/images/chihuahua.jpg', '1,1,2,2,3,3,4,4,5,5');
+INSERT INTO `heroku_b0cbb0c89762d88`.`friends` (`item_id`, `name`, `profilePic`, `scores`) VALUES ('1', 'Cool Chihuahua', 'http://www.dog-learn.com/dog-breeds/chihuahua/images/chihuahua.jpg', '1,1,2,2,3,3,4,4,5,5');
+
+
